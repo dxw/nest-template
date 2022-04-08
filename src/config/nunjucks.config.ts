00cc8ef595df236bb7546d095407de328e2c9363
pezholio <pezholio@gmail.com>
2022-04-08 14:05:18 +0100

    Move nunjucks config to a config file
    Also add a translation filter to translate in views

---
+++ src/config/nunjucks.config.ts
@@ -0,0 +1,47 @@
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import * as nunjucks from 'nunjucks';
+
+import { AssetsHelper } from '../helpers/assets.helper';
+import { I18nHelper } from '../helpers/i18n.helper';
+
+export const nunjucksConfig = async (
+  app: NestExpressApplication,
+  views: any,
+): Promise<nunjucks.ConfigureOptions> => {
+  const express = app.getHttpAdapter().getInstance();
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const entrypoints = require('../../public/entrypoints.json');
+  const assetsHelper = new AssetsHelper(entrypoints);
+  const i18nHelper = new I18nHelper(app);
+
+  const env = nunjucks.configure(views, {
+    noCache: process.env.NODE_ENV === 'development' ? true : false,
+    express: express,
+  });
+
+  env.addGlobal('encore_entry_link_tags', await assetsHelper.entryLinksTags());
+
+  env.addGlobal(
+    'encore_entry_script_tags',
+    await assetsHelper.entryScriptTags(),
+  );
+
+  env.addFilter(
+    't',
+    async (...args) => {
+      const callback = args.pop();
+      const text = args[0];
+      const personalisation = args.length < 2 ? {} : args[1];
+      try {
+        const result = await i18nHelper.translate(text, personalisation);
+        callback(null, result);
+      } catch (error) {
+        callback(error);
+      }
+    },
+    true,
+  );
+
+  return env;
+};
--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
 
-import * as nunjucks from 'nunjucks';
 import * as path from 'path';
 
 import { AppModule } from './app.module';
-import { AssetsHelper } from './helpers/assets.helper';
 import { ValidationFailedError } from './validation/validation-failed.error';
+import { nunjucksConfig } from './config/nunjucks.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const express = app.getHttpAdapter().getInstance();
-  const entrypoints = require('../public/entrypoints.json');
-  const assetsHelper = new AssetsHelper(entrypoints);
 
   const assets = path.join(__dirname, '..', 'public');
   const views = [
@@ -24,19 +20,7 @@ async function bootstrap() {
     path.join(__dirname, '..', 'node_modules', 'govuk-frontend'),
   ];
 
-  const nunjucksEnv = nunjucks.configure(views, {
-    noCache: process.env.NODE_ENV === 'local' ? true : false,
-    express: express,
-  });
-
-  nunjucksEnv.addGlobal(
-    'encore_entry_link_tags',
-    await assetsHelper.entryLinksTags(),
-  );
-  nunjucksEnv.addGlobal(
-    'encore_entry_script_tags',
-    await assetsHelper.entryScriptTags(),
-  );
+  await nunjucksConfig(app, views);
 
   app.useStaticAssets(assets);
   app.setBaseViewsDir(views);
