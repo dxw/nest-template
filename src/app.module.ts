7dfe9d20d141318084f4eddd9464b62b14e7fadc
pezholio <pezholio@gmail.com>
2022-04-08 13:53:21 +0100

    Add nestjs-i18n for translating strings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ process.env.NODE_ENV ||= 'development';
 
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { I18nModule } from 'nestjs-i18n';
+import * as path from 'path';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import dbConfiguration from './config/db.config';
 
@@ -19,6 +22,13 @@ import dbConfiguration from './config/db.config';
       useFactory: async (configService: ConfigService) => ({
         ...(await configService.get('database')),
       }),
+    }),
+    I18nModule.forRoot({
+      fallbackLanguage: 'en',
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
     }),
   ],
   controllers: [AppController],
